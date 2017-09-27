ba254e202407f2b8e06fefa9d6ce81b81aa34d70
Nikolai Bystrov <isderevni@gmail.com>
2017-09-27 12:55:56 +0300

    task4 complete

--- src/javase01/t04/Task4.java
+++ src/javase01/t04/Task4.java
@@ -1,8 +1,33 @@
 package javase01.t04;
 
+import java.util.Random;
+
+
 public class Task4 {
+
     public static void main(String[] args) {
-        double[] array = {12, 32, 4.232, -1231, 534};
+
+        Random rnd = new Random();
+        int n = rnd.nextInt(1000);
+        System.out.println("n="+n);
+        double[] array = new double[2 * n];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (0.5 - rnd.nextDouble()) * rnd.nextInt(2_000_000);
+            System.out.print(array[i]+" ");
+        }
+
+        System.out.println();
+
+        double maximum=array[0]+array[array.length-1];
+        for (int i=1;i<n;i++){
+            double tmp=array[i]+array[array.length-1-i];
+            if(tmp>maximum){
+                maximum=tmp;
+            }
+        }
+        System.out.println("maximum="+maximum);
+
 
     }
 }
