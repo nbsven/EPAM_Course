b4cfc2a9692b765341c8c5ddb1fe2ac6bce513c2
Nikolai Bystrov <isderevni@gmail.com>
2017-09-27 16:01:52 +0300

    Task 6 added javadoc

--- src/javase01/t06/Note.java
+++ src/javase01/t06/Note.java
@@ -1,30 +1,59 @@
 package javase01.t06;
 
-
+/**
+ * Класс представляющий запись в блокноте в виде строки
+ */
 public class Note {
+    /**
+     * Строка хранящая запись
+     */
     private String note;
 
+
+    /**
+     * Публичный конструктор, который инициализирует строку
+     *
+     * @param note Строка, являющаяся записью
+     */
     public Note(String note) {
         this.note = note;
     }
 
+    /**
+     * Возвращает запись в виде строки
+     *
+     * @return Строковое представление записи
+     */
     @Override
     public String toString() {
         return note;
     }
 
+    /**
+     * Проверяет на равенство двух записей
+     *
+     * @param obj Объект с которым сравнивается запись
+     * @return true, если записи эквивалентны, false, в обратном случае
+     */
     @Override
     public boolean equals(Object obj) {
-        if (obj instanceof Note) {
-            return note.equals(((Note) obj).getNote());
-        }
-        return false;
+        return obj instanceof Note && note.equals(((Note) obj).getNote());
     }
 
+    /**
+     * Метод устанавливающий записи новое содержимое
+     *
+     * @param note Новое содержимое
+     */
     public void setNote(String note) {
         this.note = note;
     }
 
+    /**
+     * Метод для получение содержимого записи в виде строки
+     *
+     * @return содержимое записи
+     */
     public String getNote() {
         return note;
     }
--- src/javase01/t06/Notebook.java
+++ src/javase01/t06/Notebook.java
@@ -1,35 +1,76 @@
 package javase01.t06;
 
 import java.util.ArrayList;
-import java.util.Collections;
 
+/**
+ * Класс имитирующий блокнот, который содержит записи. Позволяет добавлять,
+ * удалять и редактировать записи, а так же просматривать все записи.
+ */
 public class Notebook {
+    /**
+     * Массив записей в блокноте
+     */
     private ArrayList<Note> notes = new ArrayList<>(100);
 
+    /**
+     * Метод для добавления новой записи в блокнот в виде {@code note}
+     *
+     * @param note новвая запись в виде {@code note}
+     */
     public void addNote(Note note) {
         notes.add(note);
     }
 
+    /**
+     * Метод для добавления записи в виде {@code String}
+     *
+     * @param s новая записиь в виде {@code String}
+     */
     public void addNote(String s) {
         addNote(new Note(s));
     }
 
+    /**
+     * Удаление записи по самой записи
+     *
+     * @param note записиь эквивалентная которой будет удалена из блокнота
+     */
     public void deleteNote(Note note) {
         notes.remove(note);
     }
 
+    /**
+     * Удаление записи по индексу
+     *
+     * @param i индекс удаляемой записи из блокнота
+     */
     public void deleteNote(int i) {
         notes.remove(i);
     }
 
+    /**
+     * Редактирование записи, путем замены содержимого на новое
+     *
+     * @param index индекс записи, содержимое которой изменяется
+     * @param note  новое содержимое записи в виде {@code Note}
+     */
     public void editNote(int index, Note note) {
         notes.set(index, note);
     }
 
+    /**
+     * Редактирование записи, путем замены содержимого на новое
+     *
+     * @param index индекс записи, содержимое которой изменяется
+     * @param s     новое содержимое записи в виде {@code String}
+     */
     public void editNote(int index, String s) {
         notes.set(index, new Note(s));
     }
 
+    /**
+     * Метод для отображение всех записей блокнота построчно вместе с их индексами
+     */
     public void showNotes() {
         if (notes.size() == 0) {
             System.out.println("Have no notes");
