90e92d2a6606304334404310f9a64ab0ae810b2b
Nikolai Bystrov <isderevni@gmail.com>
2017-10-23 12:31:18 +0300

    Task7 complete. Created Submarine annotation.

--- OopHomework2/src/main/java/Task6/AtomicSubmarine.java
+++ OopHomework2/src/main/java/Task6/AtomicSubmarine.java
@@ -1,5 +1,11 @@
 package Task6;
 
+import Task7.Submarine;
+
+/**
+ * Atomic submarine can swim.
+ */
+@Submarine(name = "Atomic submarine")
 public class AtomicSubmarine {
 
     private EngineOfAtomicSubmarine engine;
