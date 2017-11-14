220f72982a627045e49a031b090d95eaeda3dfa8
Nikolai Bystrov <isderevni@gmail.com>
2017-11-14 23:31:49 +0300

    Homework7 Task2 creating files refactor to 1 cycle.

--- MultithreadingHomework7/src/test/java/Task2/PropertiesReaderTest.java
+++ MultithreadingHomework7/src/test/java/Task2/PropertiesReaderTest.java
@@ -13,43 +13,29 @@ class PropertiesReaderTest {
 
     @Test
     public void test() {
-        File file1 = new File("src/main/resources/Task2/file1.properties");
-        File file2 = new File("src/main/resources/Task2/file2.properties");
         Random rnd = new Random(42);
         for (int i = 0; i < 256; i++) {
             System.out.println(i + "=" + (char) i);
         }
 
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file1))) {
-            StringBuilder line = new StringBuilder();
-            for (int i = 0; i < 1000; i++) {
-                for (int j = 0; j < 12; j++) {
-                    line.append((char) (rnd.nextInt(26) + 97));
+        for (int number = 0; number < 5; number++) {//create 5 properties files
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(
+                    new File("src/main/resources/Task2/file" + number + ".properties")))) {
+                StringBuilder line = new StringBuilder();
+                for (int i = 0; i < 1000; i++) {
+                    for (int j = 0; j < 12; j++) {
+                        line.append((char) (rnd.nextInt(26) + 97));
+                    }
+                    line.append("=");
+                    line.append(rnd.nextInt(Integer.MAX_VALUE));
+                    line.append("\n");
                 }
-                line.append("=");
-                line.append(rnd.nextInt(Integer.MAX_VALUE));
-                line.append("\n");
+                writer.write(line.toString());
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-            writer.write(line.toString());
-        } catch (Exception e) {
-            e.printStackTrace();
         }
 
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file2))) {
-            StringBuilder line = new StringBuilder();
-            for (int i = 0; i < 1000; i++) {
-                for (int j = 0; j < 12; j++) {
-                    line.append((char) (rnd.nextInt(26) + 97));
-                }
-                line.append("=");
-                line.append(rnd.nextInt(Integer.MAX_VALUE));
-                line.append("\n");
-            }
-            writer.write(line.toString());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
 
-        
     }
 }
