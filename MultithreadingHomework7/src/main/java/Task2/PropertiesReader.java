5a3ac075d66df23b679bbc65599453c54ca8f30e
Nikolai Bystrov <isderevni@gmail.com>
2017-11-15 08:27:22 +0300

    Homework7 Task2 complete.

--- MultithreadingHomework7/src/main/java/Task2/PropertiesReader.java
+++ MultithreadingHomework7/src/main/java/Task2/PropertiesReader.java
@@ -1,5 +1,43 @@
 package Task2;
 
-public class PropertiesReader {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
+public class PropertiesReader {
+    private static class CallableTask implements Callable<Map<String, String>> {
+        private File file;
+
+        public CallableTask(File file) {
+            this.file = file;
+        }
+
+        @Override
+        public Map<String, String> call() throws Exception {
+            HashMap<String, String> resultMap = new HashMap<>();
+            try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    String key = line.split("=")[0];
+                    String value = line.substring(key.length() + 1);
+                    resultMap.putIfAbsent(key, value);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return resultMap;
+        }
+    }
+
+    public static Future<Map<String, String>> readProperties(File file) {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+
+        return executorService.submit(new CallableTask(file));
+    }
 }
--- MultithreadingHomework7/src/test/java/Task2/PropertiesReaderTest.java
+++ MultithreadingHomework7/src/test/java/Task2/PropertiesReaderTest.java
@@ -5,7 +5,10 @@ import org.junit.jupiter.api.Test;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
+import java.util.Map;
 import java.util.Random;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,9 +17,6 @@ class PropertiesReaderTest {
     @Test
     public void test() {
         Random rnd = new Random(42);
-        for (int i = 0; i < 256; i++) {
-            System.out.println(i + "=" + (char) i);
-        }
 
         for (int number = 0; number < 5; number++) {//create 5 properties files
             try (BufferedWriter writer = new BufferedWriter(new FileWriter(
@@ -34,6 +34,26 @@ class PropertiesReaderTest {
             } catch (Exception e) {
                 e.printStackTrace();
             }
+        }
+
+        Future[] mapFuture = new Future[5];
+        for (int i = 0; i < 5; i++) {
+
+            mapFuture[i] = PropertiesReader.readProperties(new File("src/main/resources/Task2/file" + i + ".properties"));
+        }
+
+        for (int i = 0; i < 5; i++) {
+            try {
+                System.out.println(mapFuture[i].get());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+
+        for (int i = 0; i < 5; i++) {
+            new File("src/main/resources/Task2/file" + i + ".properties").delete();
         }
 
 
